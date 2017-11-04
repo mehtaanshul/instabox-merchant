33a2dce9d2e74165f9e5cd92b1fd5b6847595948
Anshul Mehta <anshul.mk97@gmail.com>
2017-11-04 12:16:50 +0530

    minor changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,7 +41,7 @@ export default class HomeScreen extends React.Component {
           />
         </VictoryChart>*/}
         <Text>
-          Welcome Admin.
+          Welcome123 Admin.
         </Text>
       </View>
   )
